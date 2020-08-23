2eff245f14944c2c89e98d5134ef01535dc937de
aqcent <aqcent@gmail.com>
2020-08-23 17:33:47 +0500

    Updates

---
+++ precache-manifest.78504c5e53da28d40d8a6279c3ecb14c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78a33f0d4d10867bc0ed838efae39af",
+    "url": "/aqcent.github.io/fun-box-kitty/index.html"
+  },
+  {
+    "revision": "434e2866becdd1564412",
+    "url": "/aqcent.github.io/fun-box-kitty/static/css/main.d01266e5.chunk.css"
+  },
+  {
+    "revision": "275a6f18a2ba8cd5eb8c",
+    "url": "/aqcent.github.io/fun-box-kitty/static/js/2.78369f75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aqcent.github.io/fun-box-kitty/static/js/2.78369f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434e2866becdd1564412",
+    "url": "/aqcent.github.io/fun-box-kitty/static/js/main.8a990ceb.chunk.js"
+  },
+  {
+    "revision": "091b734c38e45fb867e2",
+    "url": "/aqcent.github.io/fun-box-kitty/static/js/runtime-main.ac13bd44.js"
+  },
+  {
+    "revision": "d334fe476880ad58e412bd58af6315b2",
+    "url": "/aqcent.github.io/fun-box-kitty/static/media/cat.d334fe47.png"
+  }
+]);
